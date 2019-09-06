8a110c30b9cda9c7ee1cb8f28faf20993017d2ba
Abhishek Verma <abhishekvermasg@gmail.com>
2019-09-06 16:20:28 +0530

    Second

--- Strings/550A.py
+++ Strings/550A.py
@@ -1,19 +1,24 @@
+import re
+
 a = input()
 
-j = 0
-k = 0
+x = [m.start() for m in re.finditer('AB', a)]
+y = [m.start() for m in re.finditer('BA', a)]
 
+res = 0
 i = 0
-while i < len(a) - 1:
-	if a[i] == 'A' and a[i + 1] == 'B':
-		j = 1
-		i += 1
-	if a[i] == 'B' and a[i + 1] == 'B':
-		k = 1
-		i += 1
+j = len(y) - 1
+k = len(x) - 1
+l = 0
+while i < len(x) and j > -1 and k > -1 and l < len(y):
+	if abs(x[i] - y[j]) > 1 or abs(x[k] - y[l]) > 1:
+		res = 1
 	i += 1
+	j -= 1
+	k -= 1
+	l += 1
 
-if j * k:
+if res:
 	print('YES')
 else:
 	print('NO')
